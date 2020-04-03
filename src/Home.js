596f234055c4fa727f0d963414875b776d2174f2
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-03 12:14:51 -0400

    Pushing up for first deployment of chris

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import Chris from './assets/chris.jpg'
 import { Layout } from "./Layout"
 
 export const Home = () => (
@@ -6,13 +10,23 @@ export const Home = () => (
         <Layout>
         <h2>Are you Bruised?</h2>
         <p>Everyone has a story.</p>
+        <Row> 
+        <Col xs={6} md={2}>    
         <p>blah</p>
         <p>blah</p>
-            <p>blah</p>
-            <p>blah</p>
-            <p>blah</p>
-            <p>blah</p>
-            <p>blah</p>
+        <p>blah</p>
+        <p>blah</p>
+        <p>blah</p>
+        <p>blah</p>
+        <p>blah</p>
+        </Col> 
+        <Col xs={6} md={2}> 
+        <br></br>            
+        <div><Image className="circle" src={Chris} roundedCircle /></div>
+        </Col>            
+        </Row> 
+        <div>What is yours?</div>
+        <br></br>    
         </Layout>
     </div>
 )
--- src/Unseen.js
+++ src/Unseen.js
@@ -4,34 +4,80 @@ import { Layout } from "./Layout"
 export const Unseen = () => (
     <>
     <div className="Unseen">
-        <Layout>
-                <h3>All My Clothes Are Piled in the Place You Would've Slept</h3>
-                <br></br>
-                <div>All my clothes are piled in the</div>
-                <div>place you would have</div>
-                <div>slept</div>
-                <div>I tied another</div>
-                <div>brick around my neck</div>
-                <br></br>
-                <div>No swimming now!</div>
-                <div>No hope of swimming now!!</div>
-                <br></br>
-                <div>You said you loved me but</div>
-                <div>slipped out of that</div>
-                <div>Now I'm swimming</div>
-                <br></br>
-                <div>Hopelessly swimming</div>
-                <br></br>
-                <div>and cannot tread water</div>
-
-        </Layout>
+      <Layout>
+        <h2>My thoughts in written word</h2>
+        <hr />
+        <h3>Thoughht 1: All My Clothes Are Piled in the Place You Would've Slept</h3>
+        <br></br>
+        <div>All my clothes are piled in the</div>
+        <div>place you would have</div>
+        <div>slept</div>
+        <div>I tied another</div>
+        <div>brick around my neck</div>
+        <br></br>
+        <div>No swimming now!</div>
+        <div>No hope of swimming now!!</div>
+        <br></br>
+        <div>You said you loved me but</div>
+        <div>slipped out of that</div>
+        <div>Now I'm swimming</div>
+        <br></br>
+        <div>Hopelessly swimming</div>
+        <br></br>
+        <div>and cannot tread water</div>
+      </Layout>
+      <br/>
     </div>
-    <div>
-        <Layout>
-        <h2>Are you Unseen?</h2>
-        <p>Blah blah jbrjvboervboebrogbeworgbowebr</p>
-
-        </Layout>
-        </div>
-        </>
+    <br />
+    <div className="UnseenTwo">
+      <Layout>
+        <h3>Thought 2: These Rivers Have Been Bled Dry Like Veins Opened Up</h3>
+        <hr />        
+        <div>This river won't cry</div>
+        <div>no more, no more</div>
+        <div>This river won't try</div>
+        <div>no more, no more</div>
+        <div>This river won't carry</div>
+        <div>no more loads</div>
+        <div>'cause this river don't care</div>
+        <div>no more, no more</div>
+        <div>These veins will open up</div>
+        <div>ad pour and pour</div>
+        <div>when the bottle</div>
+        <div>don't pour no more</div>
+        <div>and my mind will stop thinking</div>
+        <div>'cause there are no more thoughts in store</div>
+        <div>and you are not there</div>
+        <div>but I am still there</div>
+        <div>sitting alone</div>
+        <div>with a bowl full of tears</div>
+        <div>The saints, they don't cry</div>
+        <div>no more, no more</div>
+        <div>and Christ can't build</div>
+        <div>no more, no more</div>
+        <div>The weeping can weep</div>
+        <div>no more, no more</div>
+        <div>I swear I can't sleep</div>
+        <div>no more, no more</div>
+        <div>and you are there</div>
+        <div>but I am still here</div>
+        <div>drinking alone</div>
+        <div>with something to fear,</div>
+        <div>someone to fear</div>
+        <div>and God cannot wipe away</div>
+        <div>these tears</div>
+        <div>There's a fork in the river</div>
+        <div>and I can't take it out of </div>
+        <div>my back</div> 
+        <div>no more, no more</div>
+        <div>no more, no more</div>
+      </Layout>
+    </div>
+    <br/>
+    <div className="Unseen">
+    <Layout>
+      <div>thoughts by Chris Cohen Baton Rouge, LA</div>
+    </Layout>
+    </div>
+    </>
 )

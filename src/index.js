fb11da64d22db946705df534f2e0bcb96942f5bb
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-14 20:34:09 -0400

    getting dirty with index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MySound from "./assets/sounds/mainSound.mp3"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <React.Fragment>
+      <div>
+        <audio src={MySound} preload="auto" controls autoPlay loop />
+      </div>
+    </React.Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );

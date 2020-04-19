7019e4658d23d9507f728b21a7425e2e4e0897da
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-19 12:08:53 -0400

    applied Layout component to audio

--- src/Audio.js
+++ src/Audio.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { Layout } from "./Layout"
 import MySound from "./assets/sounds/mainSound.mp3"
 import Hart from "./assets/sounds/hartIsland.mp3"
 import Droney from "./assets/sounds/droney.mp3"
 
 export const Audio = () => (
-      <div className="Audio">
-        <div>
-          <audio src={MySound} preload="auto" controls loop /><p>Song</p>
-        </div>
-        <div>
-          <audio src={Hart} preload="auto" controls loop /><p>Hart Island story time</p>
-        </div>
-        <div>
-          <audio src={Droney} preload="auto" controls autoPlay loop /><p>Frikkle</p>
-        </div>
+    <div className="Audio">
+        <Layout>
+          <div>
+            <audio src={MySound} preload="auto" controls loop /><p>Song</p>
+          </div>
+          <div>
+            <audio src={Hart} preload="auto" controls loop /><p>Hart Island story time</p>
+          </div>
+          <div>
+            <audio src={Droney} preload="auto" controls autoPlay loop /><p>Frikkle</p>
+          </div>
+        </Layout>    
       </div>       
    )
 

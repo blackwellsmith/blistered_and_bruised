c964874ee61d01bff0493399580c7d548107e89f
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-20 09:46:18 -0400

    changed text again

--- src/Home.js
+++ src/Home.js
@@ -8,25 +8,28 @@ import { Layout } from "./Layout"
 export const Home = () => (
     <div className="Home">
         <Layout>
-        <h2>What do you do?</h2>
-        <p>Call poison control.</p>
-        <Row> 
-        <Col xs={6} md={2}>    
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        </Col> 
-        <Col xs={6} md={2}> 
-        <br></br>            
-        <div><Image className="circle" src={Chris} roundedCircle /></div>
-        </Col>            
-        </Row> 
-        <div>1-800-222-1222</div>
-        <br></br>    
+          <h2>What do you do?</h2>
+            <p>Call poison control.</p>
+            <p>1-800-222-1222</p>
+            <Row> 
+              <Col xs={6} md={2}>    
+                <p>blah</p>
+                <p>blah</p>
+                <p>blah</p>
+                <p>blah</p>
+                <p>blah</p>
+                <p>blah</p>
+                <p>blah</p>
+              </Col> 
+              <Col xs={6} md={2}> 
+                <br></br>            
+                <div>
+                  <Image className="circle" src={Chris} roundedCircle />
+                </div>
+              </Col>            
+            </Row> 
+            <div>What is yours?</div>
+            <br></br>    
         </Layout>
     </div>
 )

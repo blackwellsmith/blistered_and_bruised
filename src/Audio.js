e87739de64ae73bee03f0ddc765c836ed1b8de33
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-20 11:05:10 -0400

    turned bandcamp logo into jpeg and stored it locally

--- src/Audio.js
+++ src/Audio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout } from "./Layout"
+import BandCamp from ".assets/bandcamp.jpeg"
 import MySound from "./assets/sounds/mainSound.mp3"
 import Hart from "./assets/sounds/hartIsland.mp3"
 import Droney from "./assets/sounds/droney.mp3"
@@ -14,17 +15,17 @@ export const Audio = () => (
         <ButtonToolbar className="bt">
           <div>
             <div>
-              <audio src={MySound} preload="auto" controls loop /><p>Song</p>
+              <audio src={MySound} preload="auto" controls loop /><p>Nothing</p>
             </div>
             <div>
               <audio src={Hart} preload="auto" controls loop /><p>Hart Island</p>
             </div>
             <div>
-              <audio src={Droney} preload="auto" controls autoPlay loop /><p>Frikkle</p>
+              <audio src={Droney} preload="auto" controls autoPlay loop /><p>Droney</p>
             </div>
           </div>
         </ButtonToolbar>
-        <p>Someone's Been Poisoned <a href="https://soundcloud.com/christuco"><img src="https://connect.soundcloud.com/2/btn-connect-sc-s.png"/></a><a href="https://someonesbeenpoisoned.bandcamp.com/releases"><img src="https://bandcamp.com/img/buttons/bandcamp-logotype-light.zip" /></a></p>
+        <p>Someone's Been Poisoned <a href="https://soundcloud.com/christuco"><img src="https://connect.soundcloud.com/2/btn-connect-sc-s.png"/></a><a href="https://someonesbeenpoisoned.bandcamp.com/releases"><img src={BandCamp} /></a></p>
         <p>Sound by Chris Cohen</p>
       </Layout>    
       </div>       
